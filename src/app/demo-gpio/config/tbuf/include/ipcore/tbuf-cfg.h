bb9a591634a902d974a61b47a69d8e9e52c3d4ae
Thomas Mair <thomas.mair@br-automation.com>
2013-12-18 07:34:02 +0100

    [TASK] Move tbuf-cfg.h
    Move auto generated configuration header from appifcommon/include
    to demo configuration files. This enables to build the unit tests with
    a seperate configuration.
    
    Change-Id: I4ab58782933be1f3c90b45ed2386762608af15bf
    Signed-off-by: Thomas Mair <thomas.mair@br-automation.com>

--- src/app/demo-gpio/config/tbuf/include/ipcore/tbuf-cfg.h
+++ src/app/demo-gpio/config/tbuf/include/ipcore/tbuf-cfg.h
@@ -3,11 +3,11 @@
 // This file is generated automatically depending on the ipcore settings!
 // Hence, it is highly recommended to avoid manual modifications!
 //
-// timestamp = 2013-10-31 09:39:07
+// timestamp = 2013-12-18 14:03:02
 //-------------------------------------------------------------------------
 
-#ifndef __APPIF_CFG_H__
-#define __APPIF_CFG_H__
+#ifndef _INC_tbuf_cfg_H_
+#define _INC_tbuf_cfg_H_
 
 #define TBUF_OFFSET_CONACK 0
 #define TBUF_SIZE_CONACK 4
@@ -22,35 +22,27 @@
 #define TBUF_PORTA_ISPRODUCER1 0
 
 #define TBUF_OFFSET2 28
-#define TBUF_SIZE2 28
+#define TBUF_SIZE2 8
 #define TBUF_PORTA_ISPRODUCER2 0
 
-#define TBUF_OFFSET3 56
-#define TBUF_SIZE3 36
-#define TBUF_PORTA_ISPRODUCER3 0
+#define TBUF_OFFSET3 36
+#define TBUF_SIZE3 4
+#define TBUF_PORTA_ISPRODUCER3 1
 
-#define TBUF_OFFSET4 92
-#define TBUF_SIZE4 4
+#define TBUF_OFFSET4 40
+#define TBUF_SIZE4 12
 #define TBUF_PORTA_ISPRODUCER4 1
 
-#define TBUF_OFFSET5 96
-#define TBUF_SIZE5 12
+#define TBUF_OFFSET5 52
+#define TBUF_SIZE5 4
 #define TBUF_PORTA_ISPRODUCER5 1
 
-#define TBUF_OFFSET6 108
-#define TBUF_SIZE6 36
-#define TBUF_PORTA_ISPRODUCER6 1
-
-#define TBUF_OFFSET7 144
-#define TBUF_SIZE7 36
-#define TBUF_PORTA_ISPRODUCER7 1
-
-#define TBUF_OFFSET_PROACK 180
+#define TBUF_OFFSET_PROACK 56
 #define TBUF_SIZE_PROACK 4
 #define TBUF_PORTA_ISPRODUCER_PROACK -1
 
-#define TBUF_NUM_CON 4
-#define TBUF_NUM_PRO 4
+#define TBUF_NUM_CON 3
+#define TBUF_NUM_PRO 3
 
 #define TBUF_INIT_VEC { \
                         { TBUF_OFFSET_CONACK, TBUF_SIZE_CONACK, TBUF_PORTA_ISPRODUCER_CONACK },  \
@@ -60,9 +52,7 @@
                         { TBUF_OFFSET3, TBUF_SIZE3, TBUF_PORTA_ISPRODUCER3 },  \
                         { TBUF_OFFSET4, TBUF_SIZE4, TBUF_PORTA_ISPRODUCER4 },  \
                         { TBUF_OFFSET5, TBUF_SIZE5, TBUF_PORTA_ISPRODUCER5 },  \
-                        { TBUF_OFFSET6, TBUF_SIZE6, TBUF_PORTA_ISPRODUCER6 },  \
-                        { TBUF_OFFSET7, TBUF_SIZE7, TBUF_PORTA_ISPRODUCER7 },  \
                         { TBUF_OFFSET_PROACK, TBUF_SIZE_PROACK, TBUF_PORTA_ISPRODUCER_PROACK } \
                       }
 
-#endif
+#endif /* _INC_tbuf_cfg_H_ */
--- src/pcp/appif/nios2.cmake
+++ src/pcp/appif/nios2.cmake
@@ -120,7 +120,8 @@ IF( NOT ${FIX_RES} MATCHES "0" )
 ENDIF ( NOT  ${FIX_RES} MATCHES "0" )
 
 # Copy triple buffer configuration file
-FILE( COPY ${BSP_DIR}/tbuf-cfg.h DESTINATION ${CMAKE_SOURCE_DIR}/libs/appifcommon/include/ipcore )
+FILE( MAKE_DIRECTORY ${DEMO_CONFIG_DIR}/config/tbuf/include/ipcore )
+FILE( COPY ${BSP_DIR}/tbuf-cfg.h DESTINATION ${DEMO_CONFIG_DIR}/config/tbuf/include/ipcore )
 
 ########################################################################
 # Eclipse project files
